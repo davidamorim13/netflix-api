31b687cc891b11a5ced4380b88d13fa067e20c1d
Davi <dr4714@gmail.com>
2024-04-17 19:45:26 -0300

    criando de filmes

---
+++ src/movies/dto/create-movie.dto.ts
@@ -0,0 +1,9 @@
+export class CreateMovieDto {
+    title: string
+    category: string
+    image: string
+    year: number
+    duration: number
+    sinopse: string
+    classification: number
+}
---
+++ src/movies/movies.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
+
+@Injectable()
+export class MoviesService {
+  create(createMovieDto: CreateMovieDto) {
+    return 'This action adds a new movie';
+  }
+
+  findAll() {
+    return {
+      title: "Transformes",
+    category: "Ficção cientifica",
+    image: "foto.png",
+    year: 2023,
+    duration: 1,
+    sinopse: "Um filme de ficção cientifica",
+    classification: 10,
+    }
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} movie`;
+  }
+
+  update(id: number, updateMovieDto: UpdateMovieDto) {
+    return `This action updates a #${id} movie`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} movie`;
+  }
+}
